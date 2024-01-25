e3a858e2bb79887bc09d8b2fcbc21918a85be394
gergovari <work@varigergo.hu>
2024-01-25 14:46:05 +0100

    fix: env

--- dotenv/get_var/__main__.py
+++ dotenv/get_var/__main__.py
@@ -2,9 +2,9 @@ from dotenv import load_dotenv
 import os
 from typing import Any
 
-def get_var(key: {"widget_name": "string_entry"} = "") -> [{"name": "value", "type": str}]:
+def get_var(key: {"widget_name": "string_entry", "type": str} = "", override: {"widget_name": "check_button", "type": bool} = True) -> [{"name": "value", "type": str}]:
 	# NOTE: is this correct? with empty values it doesn't work in Nodezator, wonder where it would be searching...
-	load_dotenv(os.path.join(os.getenv('PWD'), ".env")) 
+	load_dotenv(os.path.join(os.getenv('PWD'), ".env"), override=override) 
 	return os.getenv(key)
 
 main_callable = get_var
