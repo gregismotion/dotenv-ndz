2eb2920c7936063d5cf143de2ebe3fea12ee35b0
gergovari <work@varigergo.hu>
2024-01-25 10:37:16 +0100

    fix: nodezator pwd issues

--- dotenv/get_var/__main__.py
+++ dotenv/get_var/__main__.py
@@ -3,7 +3,7 @@ import os
 from typing import Any
 
 def get_var(key: {"widget_name": "string_entry"} = "") -> [{"name": "value", "type": str}]:
-	load_dotenv()
+	load_dotenv(os.getenv("PWD")) # NOTE: is this correct? with emptry values it doesn't work in Nodezator, wonder where it would be searching...
 	return os.getenv(key)
 
 main_callable = get_var
